c5edbb198068957f2a9fd1317be7f2ba40763082
ibaragozz <baragozz1@gmail.com>
2024-09-20 12:34:44 +0300

    usage Keys

---
+++ 3rdpars.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+import time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# browser = webdriver.Chrome()
+# browser.get('https://en.wikipedia.org/wiki/Document_Object_Model')
+# # browser.save_screenshot('dom.png')
+# time.sleep(5)
+# browser.get('https://en.wikipedia.org/wiki/Selenium')
+# time.sleep(3)
+# browser.refresh()
