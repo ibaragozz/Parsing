1504262ffbf0d0a26862caba8c2cfcb7c45f9a56
ibaragozz <baragozz1@gmail.com>
2024-09-22 15:16:14 +0300

    Update divanparsnew.py

--- divanpars/divanpars/spiders/divanparsnew.py
+++ divanpars/divanpars/spiders/divanparsnew.py
@@ -7,4 +7,10 @@ class DivanparsnewSpider(scrapy.Spider):
     start_urls = ["https://www.divan.ru/category/krovati"]
 
     def parse(self, response):
-        pass
+        krovati = response.css('div._Ud0k')
+        for krovat in krovati:
+            yield {
+                'name': krovat.css('div.lsooF span::text').get()
+                'price': krovat.css('div.pY3d2 span::text').get()
+
+            }
